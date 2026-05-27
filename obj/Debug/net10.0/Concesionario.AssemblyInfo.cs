6db1d5ee3e71ef0d01f18e4a7328aae5c2d3a99d
Jonathan Gabriel Garay <103967649+Jonise1564@users.noreply.github.com>
2026-05-27 11:46:27 -0300

    up

--- obj/Debug/net10.0/Concesionario.AssemblyInfo.cs
+++ obj/Debug/net10.0/Concesionario.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Concesionario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+25fc406189345d325eebcba7a80c8d515f5cd9a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26bac70fcbf8a2863e6cd0bd4e049075940fdebc")]
 [assembly: System.Reflection.AssemblyProductAttribute("Concesionario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Concesionario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
